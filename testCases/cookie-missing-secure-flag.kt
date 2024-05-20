51c1768c8b8248f0febeae35fb7f6aa287ed2ef9
Bhavesh Sonar <bhavesh_sonar@persistent.com>
2024-05-20 20:13:26 +0530

    added rules and testCases for setSecureFlag

--- testCases/cookie-missing-secure-flag.kt
+++ testCases/cookie-missing-secure-flag.kt
@@ -1,7 +1,11 @@
 public class CookieController {
+
+    val isKotlinFun = true
+
     public fun setCookie(value: String, response: HttpServletResponse) {
         var cookie: Cookie = Cookie("cookie", value)
         // ruleid:cookie-missing-secure-flag
+        cookie.setSecure(true)
         response.addCookie(cookie)
     }
 
@@ -9,9 +13,28 @@ public class CookieController {
     public fun explicitDisable(@RequestParam value: String, response: HttpServletResponse) {
        var cookie: Cookie = Cookie("cookie", value)
         // ruleid:cookie-missing-secure-flag
-        cookie.setSecure(false)
+        cookie.setSecure(true)
         cookie.setHttpOnly(false)
         response.addCookie(cookie)
+    }
+
+    public fun explicitDisableVariable(value: String, response: HttpServletResponse) {
+        val cookie: Cookie = Cookie("cookie", value)
+        // ruleid:cookie-missing-secure-flag
+        cookie.setSecure(isKotlinFun)
+        response.addCookie(cookie)
+    }
+
+    public fun setCookieWithTryCatch(value: String, response: HttpServletResponse) {
+        try {
+            val cookie: Cookie = Cookie("cookie", value)            
+            // ruleid:cookie-missing-secure-flag
+            cookie.setSecure(true) // explicitly setting setSecure to false
+            cookie.setHttpOnly(false) 
+            response.addCookie(cookie)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
