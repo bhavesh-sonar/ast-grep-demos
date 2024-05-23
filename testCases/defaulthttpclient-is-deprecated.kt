cac49367e918908f5f881fc0163a11cf9886a909
Bhavesh Sonar <bhavesh_sonar@persistent.com>
2024-05-23 16:54:27 +0530

    defaultHttpClient

--- testCases/defaulthttpclient-is-deprecated.kt
+++ testCases/defaulthttpclient-is-deprecated.kt
@@ -1,5 +1,3 @@
-// cf. https://mkyong.com/java/the-type-defaulthttpclient-is-deprecated/
-
 package com.exampleweb.controller
 
 import org.apache.http.Header
@@ -13,17 +11,6 @@ public class WebCrawler {
     public fun crawl(args: Array<String>): Void {
         // ruleid: defaulthttpclient-is-deprecated
         val client: HttpClient = DefaultHttpClient()
-        val request: HttpGet = HttpGet("http://google.com")
-        val response: HttpResponse= client.execute(request)
-    }
-
-}
-
-public class SecureWebCrawler {
-
-    public fun crawl(args: Array<String>): Void {
-        // ok: defaulthttpclient-is-deprecated
-        val client: HttpClient = SystemDefaultHttpClient()
         val request: HttpGet = HttpGet("http://google.com")
         val response: HttpResponse= client.execute(request)
     }
