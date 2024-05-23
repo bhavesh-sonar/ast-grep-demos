1a015c72bb3613b8bdfd93f4a6a20037e55d1e5b
Bhavesh Sonar <bhavesh_sonar@persistent.com>
2024-05-23 11:41:22 +0530

    ldap-bind testCases and rules

--- testCases/anonymous-ldap-bind.kt
+++ testCases/anonymous-ldap-bind.kt
@@ -1,13 +1,39 @@
 public class Cls {
 
+    val value: String = "none" 
+
     public fun ldapBind(env: Environment): Void {
         // ruleid:anonymous-ldap-bind
         env.put(Context.SECURITY_AUTHENTICATION, "none")
         val ctx: DirContext = InitialDirContext(env)
     }
 
-    // public fun ldapBindSafe(env: Environment): Void {
-    //     env.put(Context.SECURITY_AUTHENTICATION, "simple")
-    //     var ctx = InitialDirContext(env)
-    // }
+    public fun ldapBindVarilable(env: Environment): Void {
+        // ruleid:anonymous-ldap-bind
+        env.put(Context.SECURITY_AUTHENTICATION, value)
+        val ctx: DirContext = InitialDirContext(env)
+    }
+
+    public fun ldapBindNoAuthInfo(env: Environment): Void {
+        // ruleid:anonymous-ldap-bind
+        try {
+            env.put(Context.SECURITY_AUTHENTICATION, "none")
+            val ctx: DirContext = InitialDirContext(env)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    public fun ldapBindEmptyAuthType(env: Environment): Void {
+        // ruleid:anonymous-ldap-bind
+        env.put(Context.SECURITY_AUTHENTICATION, "")
+        val ctx: DirContext = InitialDirContext(env)
+    }
+
+    public fun ldapBindAnonymousAuth(env: Environment): Void {
+        // ruleid:anonymous-ldap-bind
+        env.put(Context.SECURITY_AUTHENTICATION, "anonymous")
+        val ctx: DirContext = InitialDirContext(env)
+    }
+
 }
