5045d6c923daffe7feb3ad47f59722b3a84c7877
Bhavesh Sonar <bhavesh_sonar@persistent.com>
2024-05-27 23:12:10 +0530

    no-null-cipher rule and testCase

---
+++ testCases/no-null-cipher.kt
@@ -0,0 +1,17 @@
+import java.lang.Runtime
+
+class Cls {
+    public fun test1(plainText: String): Array<Byte> {
+        // ruleid: no-null-cipher
+        val doNothingCipher: Cipher = NullCipher()
+        val cipherText: Cipher = doNothingCihper.doFinal(plainText)
+        return cipherText
+    }
+
+    // public fun test2(plainText: String): Void {
+    //     // ok: no-null-cipher
+    //     val cipher: Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    //     val cipherText: Array<Byte> = cipher.doFinal(plainText)
+    //     return cipherText
+    // }
+}
